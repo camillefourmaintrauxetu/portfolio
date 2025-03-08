27fa5285e749722f6122e506cc528b904806eeb1
camillefourmaintrauxetu <camille.fourmaintraux.etu@univ-lille.fr>
2025-03-08 18:31:33 +0100

    Fix the link to the stage in portfolio

--- src/pages/cursus/Curriculum.tsx
+++ src/pages/cursus/Curriculum.tsx
@@ -1,9 +1,18 @@
+import { useLocation } from "react-router-dom";
 import Education from "./Education";
 import Experiences from "./Experiences";
 import Qualities from "./Qualities";
 import Skills from "./Skills";
+import { useEffect } from "react";
+import { scrollToSection } from "../../parts/nav/SubNav";
 
 export default function CurriculumPage() {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.hash) {
+      scrollToSection(location.hash.substring(1));
+    }
+  }, [location]);
     return (
       <>
         <Education/>
