d4033f4d85b2a6b26e305c69f14ab083766ee7df
camillefourmaintrauxetu <camille.fourmaintraux.etu@univ-lille.fr>
2025-03-08 18:31:45 +0100

    More css class for the contact form

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -75,13 +75,18 @@ export default function ContactForm() {
             
             <section id="section_form">
                 <form id="contact-form" onSubmit={handleSubmit}>
-                    <label htmlFor="name">Nom</label>
-                    <input id="name" name="name" type="text" placeholder="Votre nom..." required ref={lastnameRef} />
+                    <div className='next-to-each-other'>
+                        <div className='form-name'>
+                            <label htmlFor="name">Nom</label>
+                            <input id="name" name="name" type="text" placeholder="Votre nom..." required ref={lastnameRef} />
+                        </div>
+                        <div className='form-name'>
+                            <label htmlFor="forename">Prénom</label>
+                            <input id="forename" name="forename" type="text" placeholder="Votre prénom..." required ref={firstnameRef} />
+                        </div>
+                    </div>
 
-                    <label htmlFor="forename">Prénom</label>
-                    <input id="forename" name="forename" type="text" placeholder="Votre prénom..." required ref={firstnameRef} />
-
-                    <label htmlFor="mail">E-Mail</label>
+                    <label htmlFor="mail" className='protected'>E-Mail</label>
                     <input id="mail" name="mail" type="email" placeholder="Votre mail..." required ref={emailRef} />
 
                     <label htmlFor="subject">Intitulé</label>
