7f8af2180602afd57765fb70ba0e784d86d64ece
camillefourmaintrauxetu <camille.fourmaintraux.etu@univ-lille.fr>
2025-03-08 18:29:49 +0100

    New header picture

--- src/parts/Header.tsx
+++ src/parts/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header>
         <h1>Camille FOURMAINTRAUX</h1>
         <p>Étudiant en BUT Informatique</p>
-        <img src="/img/me.jpg" alt="Ma Photo" id="photoPresentation"></img>
+        <img src="/img/me_blue.png" alt="Ma Photo" id="photoPresentation"></img>
         </header>
     );
   }
